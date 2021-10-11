b22dee836e5b15489808095cb787ecb909e72982
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2021-10-11 23:58:33 +0100

    add aula2

---
+++ aula2/BASE-0-2021/src/ASTNode.java
@@ -0,0 +1,8 @@
+public interface ASTNode {
+
+    int eval();
+
+    void compile(CompileBlock c);
+	
+}
+
---
+++ aula2/BASE-0-2021/src/ASTNum.java
@@ -0,0 +1,19 @@
+public class ASTNum implements ASTNode {
+
+int val;
+
+        public int eval() { return val; }
+
+        public ASTNum(int n)
+        {
+	   val = n;
+        }
+
+        @Override
+        public void compile(CompileBlock c) {
+                // TODO Auto-generated method stub
+                c.emit("sipush " + val);
+        }
+
+}
+
