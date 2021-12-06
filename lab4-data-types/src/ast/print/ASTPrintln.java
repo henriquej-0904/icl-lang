642aa3134073ed8d2c124c1a915bb0cbdba91366
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2021-12-06 12:54:46 +0000

    Add print AST

---
+++ lab4-data-types/src/ast/print/ASTPrintln.java
@@ -0,0 +1,40 @@
+package ast.print;
+
+import ast.ASTNode;
+import compiler.MainCodeBlock;
+import util.Coordinates;
+import util.Environment;
+import values.IValue;
+import values.VVoid;
+
+public class ASTPrintln implements ASTNode
+{
+    public static final String OPERATOR = "println";
+
+    protected ASTNode node;
+
+    /**
+     * @param node
+     */
+    public ASTPrintln(ASTNode node) {
+        this.node = node;
+    }
+
+    @Override
+    public void compile(MainCodeBlock c, Environment<Coordinates> e) {
+        // TODO Auto-generated method stub
+        throw new Error("Not implemented");
+    }
+
+    @Override
+    public IValue eval(Environment<IValue> e)
+    {
+        IValue result = this.node.eval(e);
+        System.out.println(result.show());
+
+        // println returns nothing (void)
+        return VVoid.V_VOID;
+    }
+
+    
+}
