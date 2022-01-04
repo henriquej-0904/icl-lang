b4be2473969270106112485f52e54ccb9ba743a9
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2022-01-04 02:51:51 +0000

    Update Porject config

--- level2/run.sh
+++ level2/run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run --rm -it -v "$(pwd)/Expression.icl:/app/Expression.icl" icl:level2
+docker run --rm -it -v "$(pwd)/Expression.icl:/app/Expression.icl" --network host icl:level2
